90350d1149d254245fdabd29e148b5b4e2fef816
Amal <apptalid@gmail.com>
2022-06-10 17:52:10 +0300

    first commit

---
+++ main1.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template
+# Create a new instance of the Flask class called "app"
+app = Flask(__name__)
+
+
+# The "@" decorator associates this route with the function immediately following
+@app.route('/play/<num>')
+def sayhello_world(num, color="red"):
+    # Return the string 'Hello World!' as a response
+    return render_template("index.html", _num=int(num), color=str(color))
+
+
+@app.route('/play')
+def play(num=3, color="red"):
+    # Return the string 'Hello World!' as a response
+    return render_template("index.html", _num=int(num), color=str(color))
+
+
+@app.route('/play/<num>/<string:color>')
+def color(num, color):
+    # Return the string 'Hello World!' as a response
+
+    return render_template("index.html", _num=int(num), color=color)
+    # return '''
+    # <div class="box" style="--color: {{ color }};"></div>
+    # '''
+
+
+if __name__ == "__main__":   # Ensure this file is being run directly and not from a different module
+    app.run(debug=True)    # Run the app in debug mode.
